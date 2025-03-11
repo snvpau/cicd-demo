4cee8bb3541a46bcf8e796b4c96454e600a7759c
snvpau <84196376+snvpau@users.noreply.github.com>
2025-03-11 14:37:07 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+package com.example.java_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
+    }
+}
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
     }
 }
