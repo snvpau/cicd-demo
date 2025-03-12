e885cf05cfa4e35f9cb6977e1e5aecba08812d72
snvpau <84196376+snvpau@users.noreply.github.com>
2025-03-11 21:46:00 -0600

    Update appTest.java

--- appTest.java
+++ appTest.java
@@ -1,12 +1,23 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-public class AppTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AppTest {
+
+    @LocalServerPort
+    private int port;
+
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
+
     @Test
-    public void testApp() {
-        assertTrue(true);
+    void testAppIsWorking() {
+        String url = "http://localhost:" + port + "/";
+        String response = this.restTemplate.getForObject(url, String.class);
+        assertThat(response).isEqualTo("Hello World!");
     }
 }
