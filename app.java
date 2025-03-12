1066fba8cb0aa7d3e970999f4a88666e079ada94
snvpau <84196376+snvpau@users.noreply.github.com>
2025-03-11 21:46:33 -0600

    Update app.java

--- app.java
+++ app.java
@@ -1,7 +1,23 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
     }
 }
